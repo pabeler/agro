5bf7407fff7ff9b4053a6c02d004ef081d454e66
240827 <240827@edu.p.lodz.pl>
2024-12-13 02:55:28 +0100

    dolar na pln

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -1,5 +1,5 @@
 export function priceWithTrailingZerosAndDollar(price: number): string {
-    return "$" + (Math.round(price * 100) / 100).toFixed(2)
+    return (Math.round(price * 100) / 100).toFixed(2) + " PLN"
   }
 
 export const PRODUCTS_STORAGE_KEY : string = 'PRODUCTS_STORAGE_KEY';
